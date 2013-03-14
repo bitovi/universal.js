c19356a497701e6678638a2457146e8002450126
David Luecke <daff@neyeon.com>
2013-03-14 17:41:28 -0600

    Use QUnit component. Inline documentation.

--- src/core/standalone.js
+++ src/core/standalone.js
@@ -1,3 +1,6 @@
+/**
+ * @page universal.core
+ */
 define([ './shared/alias',
 	'u/object/mixin',
 	'u/lang/isArray',
@@ -15,17 +18,77 @@ define([ './shared/alias',
             isFunction, toArray, bind, keys, values, each, map, filter) {
 
 	return alias({
+		/**
+		 * @function u.extend
+		 *
+		 * extend(object, other...)
+		 */
 		extend: extend,
+		/**
+		 * @function u.isArray
+		 *
+		 * isArray(collection)
+		 */
 		isArray: isArray,
+		/**
+		 * @function u.isEmpty
+		 *
+		 * isEmpty(obj) *alias: isEmptyObject*
+		 */
 		isEmpty: isEmpty,
+		/**
+		 * @function u.isPlainObject
+		 *
+		 * isPlainObject(obj)
+		 */
 		isPlainObject: isPlainObject,
+		/**
+		 * @function u.isFunction
+		 *
+		 * isFunction(fn)
+		 */
 		isFunction: isFunction,
+		/**
+		 * @function u.toArray
+		 *
+		 * toArray(array) *alias: makeArray*
+		 */
 		toArray: toArray,
+		/**
+		 * @function u.bind
+		 *
+		 * bind(fn, context) *alias: proxy*
+		 */
 		bind: bind,
+		/**
+		 * @function u.keys
+		 *
+		 * keys(object)
+		 */
 		keys: keys,
+		/**
+		 * @function u.values
+		 *
+		 * values(object)
+		 */
 		values: values,
+		/**
+		 * @function u.each
+		 *
+		 * each(collection, iterator , context) *alias: forEach*
+		 */
 		each: each,
+		/**
+		 * @function u.map
+		 *
+		 * map(collection, iterator, context)
+		 */
 		map: map,
+		/**
+		 * @function u.filter
+		 *
+		 * filter(collection, iterator, context)
+		 */
 		filter: filter
 	});
 });
